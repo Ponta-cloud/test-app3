c9975640448bb4b9b9da63c2125ac2061d5c34db
EC2 Default User <ec2-user@ip-172-31-71-46.ec2.internal>
2020-11-16 02:24:44 +0000

    test

--- app/services/scrape_save_blueship.rb
+++ app/services/scrape_save_blueship.rb
@@ -13,18 +13,18 @@ class ScrapeSaveBlueship
     name        = page.search('//*[@id="main_content"]/div[2]/div[2]/table/tr/td/a/span').inner_text 
     title       = page.search('//*[@id="main_content"]/h1').inner_text 
     date        = page.search('//*[@id="main_content"]/div[2]/div[1]/div[1]/div[1]/p[2]').inner_text 
-    application = page.search('//*[@id="main_content"]/div[2]/div[2]/table/tr[4]/td').inner_text
-    save_elements(url, name, title, date, application)
+    deadline = page.search('//*[@id="main_content"]/div[2]/div[2]/table/tr[4]/td').inner_text
+    save_elements(url, name, title, date, deadline)
   end
   
   #groupモデルに団体名、detailモデルに詳細情報を保存する
-  def self.save_elements(url, name, title, date, application)
+  def self.save_elements(url, name, title, date, deadline)
     group = Group.where(group_name: name).first_or_initialize
     group.save
-    detail             　　　= EventDetail.where(url: url).first_or_initialize
+    detail                   = EventDetail.where(url: url).first_or_initialize
     detail.event_title       = title  
     detail.event_date        = date
-    detail.event_deadline    = application
+    detail.event_deadline    = deadline
     detail.group_id          = group.id
     detail.save
   end
