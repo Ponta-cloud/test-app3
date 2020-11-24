022a044b15c1003b5599099847ce63916e3c0740
EC2 Default User <ec2-user@ip-172-31-71-46.ec2.internal>
2020-11-24 04:43:33 +0000

    view

--- app/controllers/service_users_controller.rb
+++ app/controllers/service_users_controller.rb
@@ -8,7 +8,6 @@ class ServiceUsersController < ApplicationController
   
   def login
     @user = ServiceUser.find_by(name: params[:name],password: params[:password])
-    # @userが存在するかどうかを判定するif文を作成してください
     if @user
       @group = Group.where(group_name: params[:name])
       render("service_users/show")
@@ -16,9 +15,11 @@ class ServiceUsersController < ApplicationController
       render("service_users/login_form")
     end
   end
+  
   def new
     @user = ServiceUser.new
-  end  
+  end
+  
   def create
     user = ServiceUser.new(
       name: params[:name],
@@ -26,8 +27,9 @@ class ServiceUsersController < ApplicationController
       ) 
     user.save  
     redirect_to("/")
-     
   end
+  
   def login_form
+    
   end
 end
