d798207ce6be6b099f59b2ac40ce1ac9b16797bd
EC2 Default User <ec2-user@ip-172-31-71-46.ec2.internal>
2020-11-26 03:01:03 +0000

    Association

--- app/services/blueship_associate_login_user.rb
+++ app/services/blueship_associate_login_user.rb
@@ -1,11 +1,64 @@
-class BlueshipAssociateLoginUser < BlueshipScrapeSave
+require 'nokogiri'
+require 'open-uri'
+require 'mechanize'
+class BlueshipAssociateLoginUser
+  def run
+    last_pg_number = fetch_last_page_num
+    urls = catalog_url(last_pg_number)
+    urls.each do |urls|
+      fetch_event_title_url(urls)
+      sleep 1
+    end
+  end 
   
-#BlueshipScrapeSaveを継承し、scrape_event_detail(url)メソッドを変更
+  def fetch_event_title_url(urls)
+    link = scrape_event_title(urls)
+        link.each do |url|
+      scrape_save_event_detail(url)
+      sleep 1
+    end
+  end 
+       
+  def scrape_save_event_detail(url)
+    scrape_event_detail(url)
+    name = @name
+    associate_service_user_save(name)  
+  end
+  
+  
+  private
+  
+  def fetch_last_page_num
+    base_url = 'https://blueshipjapan.com/search/event'
+    base_dir = '/catalog?per_page='
+    
+    page = Nokogiri::HTML(open(base_url+base_dir + '0'))
+    page.css("#search_result > div > div > div > a:nth-child(5)")[0]['href'].match(/page=(\d+)/)[1].to_i
+  end  
+  
+  def catalog_url(last_pg_number)
+    link = []
+    for pg_number in 0..last_pg_number do
+      link << "https://blueshipjapan.com/search/event/catalog?per_page=#{pg_number/18*18}"
+    end
+    link.uniq
+  end
+
+  def scrape_event_title(urls)
+    link  = []
+    agent = Mechanize.new
+    page  = agent.get(urls)
+    elements = page.search('#search_result > div > div > ul > li > article > div.event_info > h2 > a')
+    elements.each do |ele|
+    link << ele.get_attribute('href')
+    end  
+    link
+  end  
+  
   def scrape_event_detail(url)
     agent = Mechanize.new
     page  = agent.get(url)
-    name        = page.search('//*[@id="main_content"]/div[2]/div[2]/table/tr/td/a/span').inner_text 
-    associate_service_user_save(name)
+    @name = page.search('//*[@id="main_content"]/div[2]/div[2]/table/tr/td/a/span').inner_text 
   end
   
   def associate_service_user_save(name)
--- app/services/moshicom_associate_login_user.rb
+++ app/services/moshicom_associate_login_user.rb
@@ -1,11 +1,35 @@
+require 'nokogiri'
+require 'open-uri'
+require 'mechanize'
 class MoshicomAssociateLoginUser < MoshicomScrapeSave
-
-#MoshicomScrapeSaveを継承し、scrape_event_detail(url)メソッドを変更
+  
+  def run
+    url = moshicom_url
+    url.each do |url|
+      scrape_save_event_detail(url)
+      sleep 1
+    end
+  end
+  
+  def scrape_save_event_detail(url)
+    scrape_event_detail(url)
+    name = @name
+    associate_service_user_save(name)  
+  end
+  
+  private
+  
+  def moshicom_url
+    links = (41000..47600).to_a.freeze
+    links.map do |i|
+    "https://moshicom.com/#{i}"
+    end  
+  end 
+  
   def scrape_event_detail(url)
     agent = Mechanize.new
     page  = agent.get(url)
-    name  = page.search('//*[@id="main"]/div[4]/div[1]/section/div[1]/div/div[2]/h2/a').inner_text     
-    associate_service_user_save(name)
+    @name  = page.search('//*[@id="main"]/div[4]/div[1]/section/div[1]/div/div[2]/h2/a').inner_text     
   end
   
   def associate_service_user_save(name)
